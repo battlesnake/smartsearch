7cb726a905b8ed76db6f76d0d72a28bb89132399
Mark K Cowan <mark@battlesnake.co.uk>
2016-11-21 00:53:43 +0000

    Demo and README added

---
+++ README.md
@@ -0,0 +1,8 @@
+Some search algorithms
+======================
+
+Exponential search, binary search, and a hybrid of the two.
+
+The hybrid exists to solve my initial problem where binary search would have been inefficient due to the time+space requirements of my predicate being O(N).
+
+The initial problem is contained in the "demo" folder.
---
+++ demo/websocket-max-payload-size.js
@@ -0,0 +1,101 @@
+'use strict';
+
+if (process.argv.length > 2) {
+
+	testSize(+process.argv[2], () => process.exit(0));
+
+} else {
+
+	const manakin = require('manakin').global;
+	const cp = require('child_process');
+
+	const smartsearch = require('../' /* 'smartsearch' */);
+
+	const subprocessTestSize = size =>
+		cp.spawnSync('node', [module.filename, size]).status === 0;
+
+	const { value, steps } = smartsearch.run(smartsearch.hybridSearch, subprocessTestSize, 1, 1 << 28, true);
+
+}
+
+/* Test function */
+function testSize(bufSize) {
+
+	const port = 32185;
+
+	const fs = require('fs');
+	const http = require('http');
+
+	const Client = require('ws');
+	const Server = Client.Server;
+
+	const httpServer = http.createServer();
+
+	const assertEquals = (actual, expect) => {
+		if (actual !== expect) {
+			throw new Error(`Assertion failed: ${actual} === ${expect}`);
+		}
+	};
+
+	const makeBuf = size => {
+		const BS = 1 << 16;
+		const buf = Buffer.alloc(size);
+		const fd = fs.openSync('/dev/urandom', 'r');
+		let pos = 0;
+		while (size > 0) {
+			let bs = size > BS ? BS : size;
+			assertEquals(fs.readSync(fd, buf, pos, bs, null), bs);
+			pos += bs;
+			size -= bs;
+		}
+		fs.closeSync(fd);
+		return buf;
+	};
+
+	const buf = makeBuf(bufSize);
+
+	httpServer.listen(port);
+
+	const verbose = str => false && console.info(str);
+
+	httpServer.on('listening', () => {
+		verbose('Server started');
+		const server = new Server({ server: httpServer });
+		const client = new Client(`ws://localhost:${port}/`);
+
+		client.on('open', () => {
+			verbose('Client: Connection opened');
+			client.send(buf);
+		});
+
+		client.on('message', buf => {
+			verbose('Client: Data received');
+			assertEquals(buf.length, bufSize);
+			client.close();
+		});
+
+		server.on('connection', ws => {
+			verbose('Server: Connection received');
+			ws.on('message', buf => {
+				verbose('Server: Data received');
+				assertEquals(buf.length, bufSize);
+				ws.send(buf);
+			});
+			ws.on('close', () => {
+				verbose('Server: Connection closed');
+			});
+		});
+
+		client.on('close', () => {
+			verbose('Client: Connection closed');
+			server.close();
+			httpServer.close();
+		});
+
+	});
+
+	httpServer.on('close', () => {
+		verbose('All done');
+	});
+
+}
